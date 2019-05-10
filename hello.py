635a3386231c64419c58f56d61e8f5a884963f9b
Artem Veremienko <temaspsxxx@gmail.com>
2019-05-10 19:23:28 +0300

    Add fix to app.config

--- hello.py
+++ hello.py
@@ -13,7 +13,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
